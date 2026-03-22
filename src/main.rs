2fbe7380c9bee5f38a45f3cf30b641a97390a904
Satyam Lal <ssatyamlal@gmail.com>
2026-03-22 12:31:10 +0530

    add: on Esc key press, exit the ratatui screen to normal terminal

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use color_eyre::eyre::{Ok, Result};
+use ratatui::{
+    DefaultTerminal,
+    crossterm::{
+        event::{self, Event},
+        terminal,
+    },
+};
 
 fn main() -> Result<()> {
     println!("Todo App using Ratatui crate!");
     color_eyre::install()?;
 
     let terminal = ratatui::init();
+    let result = run(terminal);
     ratatui::restore();
+
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        // Rendering
+
+        //Input handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        };
+    }
 
     Ok(())
 }
