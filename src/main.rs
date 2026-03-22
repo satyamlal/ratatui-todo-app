1ca65545d507e04192be6e10e58ccc4426720b44
Satyam Lal <ssatyamlal@gmail.com>
2026-03-23 00:23:27 +0530

    add foreground & background color

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,39 @@
 use color_eyre::eyre::{Ok, Result};
 use ratatui::{
     DefaultTerminal, Frame,
-    crossterm::{
-        event::{self, Event},
-        terminal,
-    },
-    widgets::{Paragraph, Widget},
+    crossterm::event::{self, Event},
+    layout::{Constraint, Layout},
+    prelude::Stylize,
+    style::Color,
+    widgets::{Block, BorderType, Paragraph, Widget},
 };
+
+#[derive(Debug, Default)]
+struct AppState {
+    items: Vec<TodoItem>,
+}
+
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool,
+    description: String,
+}
 
 fn main() -> Result<()> {
     println!("Todo App using Ratatui crate!");
+    let mut state = AppState::default();
     color_eyre::install()?;
 
     let terminal = ratatui::init();
-    let result = run(terminal);
+    let result = run(terminal, &mut state);
     ratatui::restore();
-    
     result
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, app_state: &mut AppState) -> Result<()> {
     loop {
         // Rendering
-        terminal.draw(render)?;
+        terminal.draw(|f| render(f, app_state))?;
 
         //Input handling
         if let Event::Key(key) = event::read()? {
@@ -38,6 +49,14 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame) {
+fn render(frame: &mut Frame, app_state: &AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+    Block::bordered()
+        .border_type(BorderType::Rounded)
+        .fg(Color::Yellow)
+        .bg(Color::DarkGray)
+        .render(border_area, frame.buffer_mut());
     Paragraph::new("Hello from Ratatui Todo App!").render(frame.area(), frame.buffer_mut());
 }
